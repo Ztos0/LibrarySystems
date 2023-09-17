c077b5c829d75f655cb818c70823139eed8fcfc5
iPinkys <145170165+iPinkys@users.noreply.github.com>
2023-09-17 02:48:11 -0400

    Update Transaction.java
    Fixed transaction class and added constructor

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,240 +1,144 @@
 package PACKAGE_NAME;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
-public class Library {
+public class Transaction {
 
-    private String storedUsername = "";
-    private String storedPassword = "";
+    private Date takenDate;
 
-    private ArrayList<Book> Books;// these are never used, we need to fix these
-    private ArrayList<User> Users; // these are never used, we need to fix these
+    private Date returnDate;
 
-    private LibraryUI LibUI = new LibraryUI();
+    private Date dueDate;
 
-    public Library(){
-        Books = new ArrayList<>();// add these to try and fix arrays
-        Users = new ArrayList<>();// add these to try and fix arrays
+    private char bookLost;
+
+    private double timeBookOut;
+
+    private String renterID;
+
+    private double lateFee = 0.0;
+
+    public Transaction(String renterID, Date takenDate, Date returnDate, Date dueDate, char bookLost, double timeBookOut, double lateFee) {
+        this.renterID = renterID;
+        this.takenDate = takenDate;
+        this.returnDate = returnDate;
+        this.dueDate = dueDate;
+        this.bookLost = bookLost;
+        this.timeBookOut = timeBookOut;
+        this.lateFee = lateFee;
     }
 
-    public void signUp(){
-        storedUsername = JOptionPane.showInputDialog("Enter a new username:");
-        storedPassword = passwordInput("Enter a new password: "); // the passwordInput variable makes it so its hidden when user is inputting
-        JOptionPane.showMessageDialog(null, "Sign up successful!");
-    }
-    public boolean logIn(){
-        String username = JOptionPane.showInputDialog("Enter your username:");
-        String password = passwordInput("Enter your password:");
-
-        return storedUsername.equals(username) && storedPassword.equals(password);
+    public Transaction(String renterID, String takenDateInput, String returnDateInput, String bookLost) {
     }
 
-
-
-    private void MainMenu() {
-        while (true) {
-            String[] options = {"Add Book", "Add Renter Info", "Book Info", "Renter Info", "Transaction", "Exit"};
-            int selection = JOptionPane.showOptionDialog(null, "Select an action:",
-                    "Library Manager", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-
-            switch (selection) { //each case in the selection is in order of the String[] options
-                case 0:
-                    String userID = JOptionPane.showInputDialog(null, "Enter Renter ID:");
-                    String bookName = JOptionPane.showInputDialog(null, "Enter Book Name:");
-                    String bookGenre = JOptionPane.showInputDialog(null, "Enter Book Genre:");
-                    String bookAuthor = JOptionPane.showInputDialog(null, "Enter Book Author:");
-                    String bookPublisher = JOptionPane.showInputDialog(null, "Enter Book Publisher:");
-                    String bookReleaseDate = JOptionPane.showInputDialog(null, "Enter Book release date:");
-                    String bookCostStr = JOptionPane.showInputDialog(null, "Enter Book Price:");
-                    String bookDate = JOptionPane.showInputDialog(null, "When was the book rented? 00/00/0000");
-                    // JOptionPane showInputDialog shows the input dialog option when the user selections " Add Book"
-
-                    double bookCost = Double.parseDouble(bookCostStr);
-                    Book newBook = new Book(userID,bookName, bookGenre, bookAuthor, bookPublisher, bookReleaseDate, bookCost, bookDate);
-
-                    newBook.setBookName(bookName);
-                    Books.add(newBook);// this never reach the arraylist
-                    break;
-                case 1:
-
-                    String userName = JOptionPane.showInputDialog(null, "Enter Renter Name:");
-                    if (userName == null) {
-                        //Check for cases when the user input doesn't give an input
-                    } else {
-                        String usersID = JOptionPane.showInputDialog(null, "Enter Renter ID:");
-                        String userAge = JOptionPane.showInputDialog(null, "Enter Renter Age:");
-                        String userAddress = JOptionPane.showInputDialog(null, "Enter Renter Address:");
-                        String librarianName = JOptionPane.showInputDialog(null, "Enter Librarian Name:");
-
-                        try {
-                            int userAGE = Integer.parseInt(userAge);
-                            User newUser = new User(userName, usersID, userAGE, userAddress, librarianName);
-                            Users.add(newUser);// this never reaches the arraylist
-
-                            // lets the user know the information was added successfully
-                            JOptionPane.showMessageDialog(null, "User information added successfully.");
-                        } catch (NumberFormatException e) {
-                            JOptionPane.showMessageDialog(null, "Invalid age entered. Please enter a valid number.");
-
-                        }//end catch
-                    }//end else
-                    // JOptionPane showInputDialog shows the input dialog option when the user selections " Add Renter Info"
-
-
-                    break;
-                case 2:
-                    String bookInfo = JOptionPane.showInputDialog(null, "Enter ID:");
-                    ArrayList<Book> filteredBooks = new ArrayList<>();
-
-                    for (Book book : Books){
-                        if (book.getUserID().equalsIgnoreCase(bookInfo)){
-                            filteredBooks.add(book);
-                        }
-                    }
-                    if (!filteredBooks.isEmpty()){
-                        LibUI.displayBooks(filteredBooks);
-                        LibUI.showTable();
-                    } else{
-                        JOptionPane.showMessageDialog(null, "No books were found under that ID.");
-                    }
-                    break;
-
-                case 3:
-                    String userInfo = JOptionPane.showInputDialog(null, "Enter Renter ID:");
-                    User foundUser = null;
-                    for (User user : Users) {
-                        if (user.getUserID().equalsIgnoreCase(userInfo)) {
-                            foundUser = user;
-                            break;
-                        }
-                    }
-                    if (foundUser != null) {
-                        ArrayList<User> singleUserList = new ArrayList<>();
-                        singleUserList.add(foundUser);
-                        LibUI.displayUsers(singleUserList);
-                        LibUI.showTable();
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Renter ID not found.");
-                    }
-
-
-                    break;
-                case 4:
-
-                   /* String renterID = JOptionPane.showInputDialog(null, "Enter Renter ID:");
-                    String takenDateString = JOptionPane.showInputDialog(null, "Enter Taken Date (MM/DD/YYYY):");
-                    String returnDateString = JOptionPane.showInputDialog(null, "Enter Return Date (MM/DD/YYYY):");
-                    String bookLostStr = JOptionPane.showInputDialog(null, "Was the Book Lost? (Y/N):");
-
-                    //Date takenDate = parseDate(takenDateString);
-                    //Date returnDate = parseDate(returnDateString);
-                    boolean bookLost = Boolean.parseBoolean(bookLostStr); //change boolean to a char
-
-                    Transaction transaction = new Transaction(takenDate, returnDate, null, bookLost, 0, renterID, 0);
-
-                    Date dueDate = transaction.calculateDueDate();
-                    double durationGone = transaction.timeBookOut(new Date());
-                    double lateFee = transaction.calculateLateFee();
-
-                    JOptionPane.showMessageDialog(null,
-                            "Due Date: " + dueDate +
-                                    "\nDuration Gone (in days): " + durationGone +
-                                    "\nLate Fee: $" + lateFee);
-
-                    break;
-                    */
-
-                    String renterID = JOptionPane.showInputDialog(null, "Enter Renter ID:");
-
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-
-                    String takenDateInput = JOptionPane.showInputDialog(null, "Enter taken date (MM/DD/YYYY):");
-                    Date takenDate = null;
-
-                    try {
-                        takenDate = dateFormat.parse(takenDateInput);
-                    } catch (ParseException e) {
-                        throw new RuntimeException("Invalid date format for taken date. Please use MM/DD/YYYY.");
-                    }
-
-                    String returnDateInput = JOptionPane.showInputDialog(null, "Enter return date (MM/DD/YYYY):");
-                    Date returnDate = null;
-
-                    try {
-                        returnDate = dateFormat.parse(returnDateInput);
-                    } catch (ParseException e) {
-                        throw new RuntimeException("Invalid date format for return date. Please use MM/DD/YYYY.");
-                    }
-
-                    String bookLost = JOptionPane.showInputDialog(null, "Is book lost? Y/N");
-
-                    Transaction newTransaction = new Transaction(renterID, takenDate, returnDate, bookLost);
-
-                    Date dueDate = newTransaction.calculateDueDate();
-                    double durationGone = newTransaction.timeBookOut(new Date());
-                    double lateFee = newTransaction.calculateLateFee();
-
-                    JOptionPane.showMessageDialog(null,
-                            "Due Date: " + dueDate +
-                                    "\nDuration Gone (in days): " + durationGone +
-                                    "\nLate Fee: $" + lateFee);
-
-                case 5:
-                    System.exit(0); // this exits it when they press exit
-            }
-        }
+    public Transaction(String renterID, Date takenDate, Date returnDate, String bookLost) {
     }
 
-    public static String passwordInput(String message){
-        JPasswordField passwordField = new JPasswordField();
-        Object[] obj = {message, passwordField};
-        if (JOptionPane.showConfirmDialog(null, obj, "Password", JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION){
-            return new String (passwordField.getPassword());
-        }
-        return null;
+    public String getRenterID() {
+        return renterID;
     }
 
-    public static void main(String[] args) {
-        Library library = new Library();
+    public void setRenterID(String renterID) {
+        this.renterID = renterID;
+    }
 
-        String[] options = {"Sign Up", "Log in"};
-        int choice = JOptionPane.showOptionDialog(null, "Choose an action:",
-                "Library Access", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+    public Date getTakenDate() {
+        return takenDate;
+    }
 
-        boolean isLoginSuccessful = false;
+    public void setTakenDate(Date takenDate) {
+        this.takenDate = takenDate;
+    }
 
-        while (!isLoginSuccessful) {
-            if (choice == 0) {
-                library.signUp();
-            }
+    public Date getReturnDate() {
+        return returnDate;
+    }
 
-            isLoginSuccessful = library.logIn();
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
 
-            if (!isLoginSuccessful) {
-                String[] retryOptions = {"Sign Up", "Try Again", "Exit"};
-                int retryChoice = JOptionPane.showOptionDialog(null, "Login failed. What would you like to do next?",
-                        "Login", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, retryOptions, retryOptions[0]);
+    public Date getDueDate() {
+        return dueDate;
+    }
 
-                switch (retryChoice) {
-                    case 0: // Sign Up option chosen
-                        choice = 0;
-                        break;
-                    case 1: // Try Again option chosen
-                        choice = 1;
-                        break;
-                    case 2: // Exit option chosen
-                        System.exit(0);
-                        break;
-                }
-            }
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public char getBookLost() {
+        return bookLost;
+    }
+
+    public void setBookLost(char bookLost) {
+        this.bookLost = bookLost;
+    }
+
+    public double getTimeBookOut() {
+        return timeBookOut;
+    }
+
+    public void setTimeBookOut(double timeBookOut){
+        this.timeBookOut = timeBookOut;
+    }
+
+    public double getLateFee() {
+        return lateFee;
+    }
+
+    public void setLateFee(double lateFee) {
+        this.lateFee = lateFee;
+    }
+
+    public Date calculateDueDate() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(takenDate);
+
+        int loanPeriod = 30;
+
+        calendar.add(Calendar.DAY_OF_MONTH, loanPeriod);
+
+        return calendar.getTime();
+    }
+
+    public boolean isOverdue(){
+        if (returnDate == null){
+            return false;
         }
 
-        JOptionPane.showMessageDialog(null, "Login successful!");
-        library.MainMenu();
+        return returnDate.after(calculateDueDate());
     }
 
-}//end of Library
+    public double calculateLateFee() {
+
+        double lateFeeRatePerDay = 0.10;
+
+        Date currentDate = new Date();
+
+        long daysOverdue = calculateDaysOverdue(currentDate);
+
+        double lateFee = lateFeeRatePerDay * daysOverdue;
+
+        return lateFee;
+    }
+
+    private long calculateDaysOverdue(Date currentDate) {
+
+        if (returnDate == null || returnDate.after(currentDate)) {
+
+            return 0;
+        }
+
+        long diffInMilliseconds = currentDate.getTime() - returnDate.getTime();
+        return TimeUnit.DAYS.convert(diffInMilliseconds, TimeUnit.MILLISECONDS);
+    }
+
+    public double timeBookOut(Date currentDate){
+
+        long diffInMilliseconds = currentDate.getTime() - returnDate.getTime();
+        return timeBookOut = TimeUnit.DAYS.convert(diffInMilliseconds, TimeUnit.MILLISECONDS);
+
+    }
+}
